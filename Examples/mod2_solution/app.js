057220180ddb13a497269d33c3cd037fac847cbb
Ainhoa Carbón González <acarbon@sice.com>
2016-10-10 08:43:05 +0200

    Add files via upload

--- Examples/mod2_solution/app.js
+++ Examples/mod2_solution/app.js
@@ -2,9 +2,9 @@
   'ue strict';
 
   angular.module('ShoppingListCheckOff', [])
-         .controller('ToBuyShoppingController', ToBuyShoppingController)
-         .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
-         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+ .controller('ToBuyShoppingController', ToBuyShoppingController)
+ .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+ .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
   ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
   AlreadyBoughtShoppingController.$inject= ['ShoppingListCheckOffService'];
@@ -16,26 +16,31 @@
     {
         ShoppingListCheckOffService.BuyItem(index);
     }
-    ItemsToBuy.ShoppingListEmpty = function()
-    {
-      return ShoppingListCheckOffService.ItemsToBuyCount() <= 0;
-    }
+
     ItemsToBuy.ShoppingList = function()
     {
       return ShoppingListCheckOffService.ItemsToBuy;
     }
+
+    ItemsToBuy.ShoppingListEmpty = function()
+    {
+      return ShoppingListCheckOffService.ItemsToBuyCount() <= 0;
+    }
+
   };
 
   function AlreadyBoughtShoppingController(ShoppingListCheckOffService)
   {
     var ItemsBought = this;
-    ItemsBought.PurchasedItemsEmpty= function()
-    {
-      return ShoppingListCheckOffService.ItemsBoughtCount() <= 0;
-    }
+
     ItemsBought.PurchasedList = function()
     {
       return ShoppingListCheckOffService.ItemsBought;
+    }
+
+    ItemsBought.PurchasedItemsEmpty= function()
+    {
+      return ShoppingListCheckOffService.ItemsBoughtCount() <= 0;
     }
   }
 
@@ -44,24 +49,41 @@
     var service = this;
     service.ItemsBought = [];
     service.ItemsToBuy  = [
-      { name: "milk",     quantity: 2 },
-      { name: "cake",    quantity: 8 },
-      { name: "bread",        quantity: 5 },
-      { name: "hamburguer", quantity: 4 },
-      { name: "water",     quantity: 10 },
+      {
+         name: "milk",
+         quantity: 2
+      },
+      {
+        name: "cake",
+        quantity: 8
+      },
+      {
+        name: "bread",
+        quantity: 5
+      },
+      {
+        name: "hamburguer",
+        quantity: 4
+      },
+      {
+        name: "water",
+        quantity: 10
+      },
     ];
     service.BuyItem = function(index)
     {
       service.ItemsBought.push(service.ItemsToBuy[index]);
       service.ItemsToBuy.splice(index, 1);
     }
-    service.ItemsToBuyCount = function()
-    {
-      return service.ItemsToBuy.length;
-    }
+
     service.ItemsBoughtCount = function()
     {
       return service.ItemsBought.length;
+    }
+
+    service.ItemsToBuyCount = function()
+    {
+      return service.ItemsToBuy.length;
     }
   }
 })();
