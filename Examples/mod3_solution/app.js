6b20587894b7a14df8321a0f8286a224956b5e3e
Ainhoa Carbón González <acarbon@sice.com>
2016-10-16 20:04:02 +0200

    Add files via upload

--- Examples/mod3_solution/app.js
+++ Examples/mod3_solution/app.js
@@ -1,26 +1,95 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController )
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('menuItemsURL', "https://acarbon.github.io/coursera-test/Examples/mod3_solution/menuitems.json");;
 
-.controller('LunchCheckController', LunchCheckController);
-
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController ($scope){
-
-	$scope.lunch = "";
-	$scope.message = "";
-	
-	$scope.check = function () {
-		if ($scope.lunch != "") {
-			var arrayLunch = $scope.lunch.split(",");
-			if (arrayLunch.length <=3) {
-				$scope.message = "Enjoy!";
-			}
-			else $scope.message = "Too much!";
-		}
-		else $scope.message = "Please enter data first";
-	};
+function FoundItemsDirective(){
+  var ddo = {
+    restrict: 'E',
+    templateUrl: 'founditems.html',
+    scope:{
+      items: '<foundItems',
+      onRemove: '&'
+    },
+    controller: FoundIemsDirectiveController,
+    controllerAs: 'foundItemsCtrl',
+    bindToController: true
+  }
+  return ddo;
 }
-	
+
+function FoundIemsDirectiveController(){
+  var foundItemsCtrl = this;
+
+  foundItemsCtrl.noItems = function (){
+
+    if(foundItemsCtrl.items){
+      return foundItemsCtrl.items.length===0;
+    }
+
+    return true;
+  }
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController (MenuSearchService) {
+
+  var menu = this;
+  menu.searchTerm = '';
+
+
+  menu.getMatchedMenuItems = function (searchTerm) {
+    var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+    if(searchTerm===''){
+      menu.found=[];
+      return;
+    }
+
+    promise.then(function (response) {
+      menu.found = response;
+    })
+    .catch(function (error) {
+      console.log('Something really bad happened!');
+    })
+  };
+
+  menu.removeItem = function(indexItem){
+    menu.found.splice(indexItem,1);
+  }
+}
+
+MenuSearchService.$inject = ['$http', 'menuItemsURL','$q']
+function MenuSearchService($http, menuItemsURL,$q) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+
+    searchTerm=searchTerm.toLowerCase();
+
+
+    return $http({method: "GET",url: (menuItemsURL)}).then(function (result){
+      //process result and only keep items that match
+      var foundItems=[];
+      var numberOfItems = result.data.menu_items.length;
+      var item;
+
+      for(var i=0; i < numberOfItems; i++){
+        item=result.data.menu_items[i];
+        if(item.description.toLowerCase().indexOf(searchTerm)!==-1){
+          foundItems.push(item);
+        }
+      }
+
+    return foundItems;
+
+    })
+  };
+}
+
+
 })();
