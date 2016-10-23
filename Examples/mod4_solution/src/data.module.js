b03ab098d4c1143c5fe7711cfbf472d239d0bbac
Ainhoa Carbón González <acarbon@sice.com>
2016-10-23 20:11:02 +0200

    Add files via upload

---
+++ Examples/mod4_solution/src/data.module.js
@@ -0,0 +1,7 @@
+(function () {
+'use strict';
+
+//module creation
+angular.module('Data', []);
+
+})();
---
+++ Examples/mod4_solution/src/menuapp.module.js
@@ -0,0 +1,7 @@
+(function () {
+'use strict';
+
+//module creation, ui.router, Data injected
+angular.module('MenuApp', ['ui.router', 'Data']);
+
+})();
